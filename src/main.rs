9812b83d7783d1fc6ca6ec51bed0a354c8dc8747
Fanty1107 <edufantinlemes@gmail.com>
2026-09-11 12:08:35 -0300

    add:basic struct of keylogger

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
-use std::fs::{self, OpenOptions};
-use std::io::Write;
+use std::io::{self, Write};
+
+use crossterm::{
+    event::{Event, KeyCode, KeyModifiers, read},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
 
 fn main() -> std::io::Result<()> {
-    let file = OpenOptions::new()
-        .write(true)
-        .read(true)
-        .append(true)
-        .open("test.txt");
+    enable_raw_mode()?;
+    let mut buffer = String::new();
+    let limit_reset_buffer = 100;
 
-    let mut content = String::new();
-    fs::read_to_string(&mut content)?;
-    write!(file?, "{} is a piece of std", content)?;
-    println!("{content}");
+    loop {
+        if let Event::Key(event) = read()? {
+            if event.modifiers == KeyModifiers::CONTROL && event.code == KeyCode::Char('c') {
+                break;
+            }
+            if let KeyCode::Char(c) = event.code {
+                buffer.push(c);
+                print!("{}", c);
+                io::stdout().flush()?;
+
+                if buffer.len() >= limit_reset_buffer {
+                    println!("cleaning buffer: {}", buffer);
+                    buffer.clear();
+                    println!("{limit_reset_buffer}");
+                }
+            }
+        }
+    }
+    disable_raw_mode()?;
     Ok(())
 }
